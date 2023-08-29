d68e9bc5e5f04fd85a2f30f1f069886590f5d97a
Joao Lucas Soares <joaolucassoaresk@outlook.com>
2023-08-28 21:30:14 -0300

    CORS added(test phase), update .gitignore

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, url_for, redirect, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
-
 db = SQLAlchemy(app)
 
 class Pessoa(db.Model):
